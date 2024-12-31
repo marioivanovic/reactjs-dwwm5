ffd3dce97909e499a56c77cbcf2d7eef9f13bc3e
mrmarioi <ivanovic.mario@gmail.com>
2024-12-31 01:20:29 +0100

    :fire: first react push, props, hooks :fire:

--- src/components/toDo/toDo.js
+++ src/components/toDo/toDo.js
@@ -1,40 +1,11 @@
 import React, { useState, useRef } from 'react'
 
 function ToDo() {
-    const [tasks, setTasks] = useState([]);
-    const [newTask, setNewTask] = useState('');
-    const inputRef = useRef(null);
 
-    const addNewTask = (e) => {
-        e.preventDefault();
-
-        if (newTask.trim()) {
-            setTasks([...tasks, {
-                id: Date.now(),
-                text: newTask,
-            }]);
-            setNewTask('');
-
-            inputRef.current.focus();
-
-        }
-    }
     return (
         <>
 
-            <form onSubmit={addNewTask}>
-                <input ref={inputRef} type="text" name="" value={newTask} onChange={(e) => setNewTask(e.target.value)} placeholder="add a new task" />
-                <button type="submit">Add new task</button>
-            </form>
-
-            <ul>
-                {tasks.map(task => (
-                    <li key={task.id}>
-                        <p>{task.text}</p>
-                    </li>
-                ))}
-            </ul>
-
+        <h1>Good luck guys ! 😉</h1>
 
         </>
     )
